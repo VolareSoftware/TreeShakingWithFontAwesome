58a9b9430c10a728d9a68fa24578398c43e07401
Joe Wilson <joe@volaresystems.com>
2018-01-28 18:41:58 -0700

    initial version using FontAwesome through the CDN

---
+++ src/TreeShakingWithFontAwesome/Web/webpack.config.js
@@ -0,0 +1,59 @@
+﻿const webpack = require("webpack");
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports = () => {   
+    return {
+        entry: {
+            index: "index.js"
+        },
+        output: {
+            filename: "[name].js",
+            path: path.resolve(__dirname, "dist"),
+            publicPath: "/dist"
+        },
+        devtool: "source-map",
+        plugins: [
+            new CleanWebpackPlugin(["dist"]),
+            new webpack.optimize.CommonsChunkPlugin({
+                name: "lib"
+            }),
+            new UglifyJsPlugin({
+                sourceMap: true,
+                uglifyOptions: {
+                    dead_code: true
+                }
+            }),
+            new BundleAnalyzerPlugin()
+        ],
+        resolve: {
+            extensions: [
+                ".js"
+            ],
+            modules: [
+                path.resolve(__dirname, "scripts"),
+               "node_modules"
+            ]
+        },
+        module: {
+            rules: [
+               {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["env"],
+                            babelrc: false,
+                            cacheDirectory: true,
+                            plugins: [
+                                "transform-runtime"]
+                        }
+                    }
+                }
+            ]
+        }
+    };
+};
